ca0c5d8d2c4ecee3056d20f1305883fb3a389d95
Shakeel Hoosain <shakeelhoosaincode@gmail.com>
2024-07-30 19:29:03 +0200

    Initial commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // api/users
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        try{
+            var users = await context.Users.ToListAsync();
+            return Ok(users);
+        }
+        catch{
+            return BadRequest("Error");
+        }
+    }
+
+    [HttpGet("{id:int}")] // api/users/1
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        try{
+            var user = await context.Users.FindAsync(id);
+
+            if(user == null) return NotFound();
+            
+            return Ok(user);
+        }
+        catch{
+            return BadRequest("Error");
+        }
+    }
+}
