afdf5769bd498083fd5df9b415d3d3368c39fe4b
Shakeel Hoosain <shakeelhoosaincode@gmail.com>
2024-08-07 17:57:05 +0200

    Extended the functionality of the API

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -7,10 +7,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(DataContext context) : BaseApiController
+public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
 {
     [HttpPost("register")] // api/account/register
-    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if(await UserExists(registerDto.UserName))
         {
@@ -29,7 +29,46 @@ public class AccountController(DataContext context) : BaseApiController
         context.Users.Add(user);
         await context.SaveChangesAsync();
 
-        return user;
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+        };
+    }
+
+    [HttpPost("login")] // api/account/login
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        //Find the user
+        var user = await context.Users.FirstOrDefaultAsync(
+            x => x.UserName == loginDto.Username.ToLower()); //Tom is the same as tom
+        
+        //Validate the user
+        if(user == null)
+        {
+            return Unauthorized("Invalid Username");
+        }
+
+        //Create a new hmac object with the password salt
+        using var hmac = new HMACSHA512(user.PasswordSalt); //We using "using" to dispose of the hmac object after we are done with it
+
+        //Compute the hash of the password
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        //Validate the password by comparing the computed hash with the password hash
+        for(int i = 0; i < computedHash.Length; i++)
+        {
+            if(computedHash[i] != user.PasswordHash[i])
+            {
+                return Unauthorized("Invalid Password");
+            }
+        }
+
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+        };
     }
 
     private async Task<bool> UserExists(string username)
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using API.Data;
 using API.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,6 +8,7 @@ namespace API.Controllers;
 
 public class UsersController(DataContext context) : BaseApiController
 {
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
@@ -19,6 +21,7 @@ public class UsersController(DataContext context) : BaseApiController
         }
     }
 
+    [Authorize]
     [HttpGet("{id:int}")] // api/users/1
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
